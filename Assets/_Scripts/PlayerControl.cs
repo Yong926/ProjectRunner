37ccbe970c6c5770ada346b576ebc4a75b1560f1
Yong926 <leechangyong926@gmail.com>
2025-02-13 15:17:01 +0900

    v0.0.7] feat : DOTweenMovingJumping

--- Assets/_Scripts/PlayerControl.cs
+++ Assets/_Scripts/PlayerControl.cs
@@ -1,65 +1,56 @@
 using UnityEngine;
 using Unity.Mathematics;
+using DG.Tweening;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] float speed;
-    [SerializeField] AnimationCurve jumpCurve;
+    [SerializeField] float moveDuration = 0.5f;
+    [SerializeField] Ease moveEase;
     [SerializeField] float jumpDuration = 0.5f;
     [SerializeField] float jumpHeight = 3f;
+    [SerializeField] Ease jumpEase;
 
     [HideInInspector] public TrackManager trackMgr;
 
     private int currentLane = 1;
     private Vector3 targetpos;
-    private float jumpStarttime;
-    private bool isJumping = false;
+    private bool isMoving = false;
 
     void Update()
     {
-        if (Input.GetButtonDown("Left") && isJumping == false)
-            HandlePlayer(-1);
+        if (Input.GetButtonDown("Left") && isMoving == false)
+            HandleDirection(-1);
 
-        else if (Input.GetButtonDown("Right") && isJumping == false)
-            HandlePlayer(+1);
+        else if (Input.GetButtonDown("Right") && isMoving == false)
+            HandleDirection(+1);
 
-        else if (Input.GetButtonDown("Jump") && isJumping == false)
-        {
-            jumpStarttime = Time.time;
-            isJumping = true;
-        }
+        else if (Input.GetButtonDown("Jump") && isMoving == false)
+            HandleJump();
 
-        if (isJumping == true)
-        {
-            float elapsed = Time.time - jumpStarttime;
-            float p = Mathf.Clamp(elapsed / jumpDuration, 0f, 1f);
-            float height = jumpCurve.Evaluate(p) * jumpHeight;
-
-            targetpos = new Vector3(targetpos.x, height, targetpos.z);
-
-            if (p >= 1f)
-                isJumping = false;
-        }
-
-        UpdatePosition();
     }
 
-    void HandlePlayer(int direction)
+    void HandleDirection(int direction)
     {
+        isMoving = true;
+
         currentLane += direction;
         currentLane = math.clamp(currentLane, 0, trackMgr.laneList.Count - 1);
 
         Transform l = trackMgr.laneList[currentLane];
 
         targetpos = new Vector3(l.position.x, transform.position.y, transform.position.z);
+
+        transform.DOMove(targetpos, moveDuration)
+                .OnComplete(() => isMoving = false)
+                .SetEase(moveEase);
     }
 
-    void UpdatePosition()
+    void HandleJump()
     {
-        transform.position = Vector3.Lerp(transform.position, targetpos, speed * Time.deltaTime);
-    }
+        isMoving = true;
 
-    // void UpdateJump()
-    // {
-    // }
+        transform.DOLocalJump(targetpos, jumpHeight, 1, jumpDuration)
+                .OnComplete(() => isMoving = false)
+                .SetEase(jumpEase);
+    }
 }
