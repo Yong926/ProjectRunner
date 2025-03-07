a6f7c4cc04d03a4d73b657db25df5c5bd85426a6
Yong926 <leechangyong926@gmail.com>
2025-03-07 10:00:17 +0900

    v0.1.35] feat : Text Version UI

--- Assets/_Scripts/Scenes/SceneStart.cs
+++ Assets/_Scripts/Scenes/SceneStart.cs
@@ -1,8 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SceneStart : MonoBehaviour
 {
+
+    [SerializeField] TextMeshProUGUI tmVersion;
+
+    void OnValidate()
+    {
+        if (tmVersion != null)
+            tmVersion.text = $"v {Application.version}";
+    }
+
     public void TapToStart()
     {
         SceneManager.LoadScene(1, LoadSceneMode.Single);
